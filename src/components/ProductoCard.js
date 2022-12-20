3183a12e96b713d2a7193df4c7fbce1987fbbd10
jumabo <jumabo1005@gmail.com>
2022-12-19 22:09:45 -0500

    2do Commit

---
+++ src/components/ProductoCard.js
@@ -0,0 +1,15 @@
+import Card from 'react-bootstrap/Card';
+
+function ProductoCard({producto}) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={producto.imagen} />
+      <Card.Body>
+        <Card.Title>{producto.titulo}</Card.Title>
+        <Card.Text> ${producto.precio}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProductoCard;
---
+++ src/connections/endpoints.js
@@ -0,0 +1,5 @@
+const API = "https://ac8ed247-e1b5-48a3-91aa-bec91d03c302.mock.pstmn.io"
+
+export const PRODUCTOSCREADOS_GET_ENDPOINT = API + "/producto"
+
+export const CREARPRODUCTO_POST_ENDPOINT = API + "/producto"
